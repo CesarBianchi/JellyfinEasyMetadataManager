dcc6197ec908854823f5d7a473eacea0e32162c1
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-24 23:18:21 -0300

    added javaDoc

--- src/main/java/com/lariflix/jemm/jemm.java
+++ src/main/java/com/lariflix/jemm/jemm.java
@@ -20,11 +20,19 @@ public class Jemm {
 
     static JellyfinInstanceDetails instanceData = new JellyfinInstanceDetails();
     
+    /**
+     * The main entry point for the application.
+     * This method creates a new LoginWindow and makes it visible.
+     * 
+     * @param args An array of command-line arguments for the application.
+     * @author Cesar Bianchi
+     * @since 1.0
+     * @createdate 2022-02-01
+     */
     public static void main(String[] args) {
-        
         LoginWindow loginFrame = new LoginWindow(instanceData);
         loginFrame.setLocationRelativeTo(null);
         loginFrame.setVisible(true);
-        
     }
+    
 }
