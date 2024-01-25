fa0c3b9f27171842407734e34e7c721a990c1a47
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-25 14:17:49 -0300

    added javaDoc

--- src/main/java/com/lariflix/jemm/dtos/JellyfinCadStudioItem.java
+++ src/main/java/com/lariflix/jemm/dtos/JellyfinCadStudioItem.java
@@ -4,8 +4,17 @@ package com.lariflix.jemm.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
- *
- * @author cesarbianchi
+ * This class represents a studio item in the Jellyfin catalog.
+ * It is used to store and manage information related to a specific studio item.
+ * 
+ * @author Cesar Bianchi
+ * @since 1.0
+ */
+/**
+ * Represents a Jellyfin Cad Studio item.
+ * 
+ * @author Cesar Bianchi
+ * @since 1.0
  */
 public class JellyfinCadStudioItem {
     @JsonProperty("Name") 
@@ -21,56 +30,129 @@ public class JellyfinCadStudioItem {
     @JsonProperty("LocationType") 
     public String locationType;
 
+    /**
+     * Constructs a new JellyfinCadStudioItem object.
+     */
     public JellyfinCadStudioItem() {
     }
     
-      public String getName() {
+    /**
+     * Gets the name of the Jellyfin Cad Studio item.
+     * 
+     * @return the name of the item
+     * @since 1.0
+     */
+    public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the Jellyfin Cad Studio item.
+     * 
+     * @param name the name of the item
+     * @since 1.0
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets the server ID of the Jellyfin Cad Studio item.
+     * 
+     * @return the server ID of the item
+     * @since 1.0
+     */
     public String getServerId() {
         return serverId;
     }
 
+    /**
+     * Sets the server ID of the Jellyfin Cad Studio item.
+     * 
+     * @param serverId the server ID of the item
+     * @since 1.0
+     */
     public void setServerId(String serverId) {
         this.serverId = serverId;
     }
 
+    /**
+     * Gets the ID of the Jellyfin Cad Studio item.
+     * 
+     * @return the ID of the item
+     * @since 1.0
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the Jellyfin Cad Studio item.
+     * 
+     * @param id the ID of the item
+     * @since 1.0
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * Gets the channel ID of the Jellyfin Cad Studio item.
+     * 
+     * @return the channel ID of the item
+     * @since 1.0
+     */
     public Object getChannelId() {
         return channelId;
     }
 
+    /**
+     * Sets the channel ID of the Jellyfin Cad Studio item.
+     * 
+     * @param channelId the channel ID of the item
+     * @since 1.0
+     */
     public void setChannelId(Object channelId) {
         this.channelId = channelId;
     }
 
+    /**
+     * Gets the type of the Jellyfin Cad Studio item.
+     * 
+     * @return the type of the item
+     * @since 1.0
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Sets the type of the Jellyfin Cad Studio item.
+     * 
+     * @param type the type of the item
+     * @since 1.0
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     * Gets the location type of the Jellyfin Cad Studio item.
+     * 
+     * @return the location type of the item
+     * @since 1.0
+     */
     public String getLocationType() {
         return locationType;
     }
 
+    /**
+     * Sets the location type of the Jellyfin Cad Studio item.
+     * 
+     * @param locationType the location type of the item
+     * @since 1.0
+     */
     public void setLocationType(String locationType) {
         this.locationType = locationType;
     }
-    
-    
 }
--- src/main/java/com/lariflix/jemm/dtos/JellyfinCadStudioItems.java
+++ src/main/java/com/lariflix/jemm/dtos/JellyfinCadStudioItems.java
@@ -4,8 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
 /**
- *
- * @author cesarbianchi
+ * This class represents a collection of studio items in the Jellyfin catalog.
+ * It is used to store and manage a list of JellyfinCadStudioItem objects.
+ * 
+ * @author Cesar Bianchi
+ * @since 1.0
+ * @see JellyfinCadStudioItem
  */
 public class JellyfinCadStudioItems {
     
@@ -16,35 +20,77 @@ public class JellyfinCadStudioItems {
     @JsonProperty("StartIndex") 
     public int startIndex;
 
+    /**
+     * Default constructor for JellyfinCadStudioItems.
+     */
     public JellyfinCadStudioItems() {
     }
 
+    /**
+     * Retrieves the list of JellyfinCadStudioItem objects.
+     * 
+     * @return The list of JellyfinCadStudioItem objects.
+     * @author Cesar Bianchi
+     * @since 1.0
+     * 
+     */
     public ArrayList<JellyfinCadStudioItem> getItems() {
         return items;
     }
 
+    /**
+     * Sets the list of JellyfinCadStudioItem objects.
+     * 
+     * @param items The list of JellyfinCadStudioItem objects.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public void setItems(ArrayList<JellyfinCadStudioItem> items) {
         this.items = items;
     }
 
+    /**
+     * Retrieves the total record count.
+     * 
+     * @return The total record count.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public int getTotalRecordCount() {
         return totalRecordCount;
     }
 
+    /**
+     * Sets the total record count.
+     * 
+     * @param totalRecordCount The total record count.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public void setTotalRecordCount(int totalRecordCount) {
         this.totalRecordCount = totalRecordCount;
     }
 
+    /**
+     * Retrieves the start index.
+     * 
+     * @return The start index.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public int getStartIndex() {
         return startIndex;
     }
 
+    /**
+     * Sets the start index.
+     * 
+     * @param startIndex The start index.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public void setStartIndex(int startIndex) {
         this.startIndex = startIndex;
     }
-    
-    
-  
-    
-    
+        
 }
