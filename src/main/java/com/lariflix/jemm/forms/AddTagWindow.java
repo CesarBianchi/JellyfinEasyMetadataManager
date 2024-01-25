e647c3a509c7e36fdab82bc0c150817408fcdd81
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-24 22:16:06 -0300

    added javadoc

--- src/main/java/com/lariflix/jemm/forms/AddTagWindow.java
+++ src/main/java/com/lariflix/jemm/forms/AddTagWindow.java
@@ -2,14 +2,26 @@
 package com.lariflix.jemm.forms;
 
 /**
- *
- * @author cesarbianchi
+ * A window for adding tags.
+ * 
+ * @author Cesar Bianchi
  */
 public class AddTagWindow {
 
+    /**
+     * Constructs a new AddTagWindow.
+     * 
+     * @author Cesar Bianchi
+     */
     public AddTagWindow() {
     }
     
+    /**
+     * Displays a dialog to prompt the user for a tag name.
+     * 
+     * @return the tag name entered by the user
+     * @author Cesar Bianchi
+     */
     public String showDialog(){
         
         String cNewTag = javax.swing.JOptionPane.showInputDialog("Tag Name ?");
