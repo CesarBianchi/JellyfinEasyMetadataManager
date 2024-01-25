982cf7a1e2b911eeff49a6fd43c983f98ef98c81
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-25 18:48:57 -0300

    added javaDoc

--- src/main/java/com/lariflix/jemm/dtos/JellyfinItems.java
+++ src/main/java/com/lariflix/jemm/dtos/JellyfinItems.java
@@ -4,10 +4,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
 /**
+ * This class represents the items in Jellyfin.
  *
- * @author cesarbianchi
+ * @author Cesar Bianchi
+ * @since 1.0
+ * @see JellyfinPeopleItem
+ * @see JellyfinStudioItem
+ * @see JellyfinGenreItem
  */
 public class JellyfinItems {
+
     @JsonProperty("Items") 
     public ArrayList<JellyfinItem> items;
     @JsonProperty("TotalRecordCount") 
@@ -15,33 +21,79 @@ public class JellyfinItems {
     @JsonProperty("StartIndex") 
     public int startIndex;
 
+    p    /**
+     * Default constructor for JellyfinItems.
+     *
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public JellyfinItems() {
     }
 
+    /**
+     * Returns the items in Jellyfin.
+     *
+     * @return The items in Jellyfin.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public ArrayList<JellyfinItem> getItems() {
         return items;
     }
 
+    /**
+     * Sets the items in Jellyfin.
+     *
+     * @param items The new items in Jellyfin.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setItems(ArrayList<JellyfinItem> items) {
         this.items = items;
     }
 
+    /**
+     * Returns the total record count.
+     *
+     * @return The total record count.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public int getTotalRecordCount() {
         return totalRecordCount;
     }
 
+    /**
+     * Sets the total record count.
+     *
+     * @param totalRecordCount The new total record count.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setTotalRecordCount(int totalRecordCount) {
         this.totalRecordCount = totalRecordCount;
     }
 
+    /**
+     * Returns the start index.
+     *
+     * @return The start index.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public int getStartIndex() {
         return startIndex;
     }
 
+    /**
+     * Sets the start index.
+     *
+     * @param startIndex The new start index.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setStartIndex(int startIndex) {
         this.startIndex = startIndex;
     }
-    
-    
     
 }
