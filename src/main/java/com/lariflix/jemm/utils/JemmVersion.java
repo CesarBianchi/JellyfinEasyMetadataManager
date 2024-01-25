dc3db4e5908af7f1ab2cd76758c591ebac0c7944
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-24 22:07:34 -0300

    added JavaDoc

--- src/main/java/com/lariflix/jemm/utils/JemmVersion.java
+++ src/main/java/com/lariflix/jemm/utils/JemmVersion.java
@@ -1,21 +1,29 @@
 
 package com.lariflix.jemm.utils;
 
-
+/**
+ * Represents the version of Jemm (Jellyfin Easy Metadata Manager).
+ * @author Cesar Bianchi
+ */
 public class JemmVersion {
     private String version = new String();
 
+    /**
+     * Retrieves the current version of Jemm.
+     * 
+     * @return The version of Jemm.
+     */
     public String getVersion() {
-        
         this.setVersion("v1.0.2 - (beta)");
-        
         return version;
     }
 
+
+    /**
+     * Sets the version of the JemmVersion object.
+     * @param version the version to set
+     */
     private void setVersion(String version) {
         this.version = version;
     }
-    
-    
-    
 }
