5eedbd5e7e3762a203f95fc5d10b94c05775d693
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-24 23:30:42 -0300

    addedd javaDoc

--- src/main/java/com/lariflix/jemm/forms/AddPeopleWindow.java
+++ src/main/java/com/lariflix/jemm/forms/AddPeopleWindow.java
@@ -9,18 +9,26 @@ import javax.swing.JDialog;
 import javax.swing.SwingUtilities;
 
 /**
- * Represents a window for adding people in the JEMM application.
+ * This class represents a window for adding people in the application.
+ * It provides the necessary interface and methods to handle user interactions for adding new people.
+ * 
  * @author Cesar Bianchi
+ * @since 1.0
+ * @createdate 2022-02-01
  */
 public class AddPeopleWindow {
     JDialog dialog = null; 
     AddPeoplePanel panel = new AddPeoplePanel(); 
     
     /**
-     * Constructs a new AddPeopleWindow object with the specified ConnectJellyfinAPI data.
+     * Constructs a new AddPeopleWindow.
+     * This constructor sets the instance of ConnectJellyfinAPI used by the AddPeoplePanel and initializes the dialog.
+     * If the dialog has not been initialized yet, it creates a new JDialog with the title "JEMM - Add People", sets its size, adds the panel to it, packs it, and centers it on the screen.
      * 
-     * @param lData the ConnectJellyfinAPI data to be used in the window
+     * @param lData The instance of ConnectJellyfinAPI to be used.
      * @author Cesar Bianchi
+     * @since 1.0
+     * @createdate 2022-02-01
      */
     public AddPeopleWindow(ConnectJellyfinAPI lData) {
         
@@ -39,10 +47,14 @@ public class AddPeopleWindow {
     }
     
     /**
-     * Displays the dialog window and allows the user to define a new person.
+     * Displays the dialog to the user and returns the new person defined by the user.
+     * This method makes the dialog visible to the user and waits for the user to define a new person.
+     * After the user has defined a new person, it creates a new JellyfinPeopleItem, sets its id, name, type, and role based on the text in the text fields and the selected item in the combo box, and returns it.
      * 
-     * @return the newly defined JellyfinPeopleItem object
+     * @return The new person defined by the user.
      * @author Cesar Bianchi
+     * @since 1.0
+     * @createdate 2022-02-01
      */
     public JellyfinPeopleItem showDialog(){
         
@@ -62,8 +74,11 @@ public class AddPeopleWindow {
     /**
      * Returns the dialog associated with this AddPeopleWindow.
      * 
-     * @return the JDialog object representing the dialog
+     * @return the JDialog object representing the dialog associated with this AddPeopleWindow object
+     * @see JDialog
      * @author Cesar Bianchi
+     * @since 1.0
+     * @createdate 2022-02-01
      */
     public JDialog getDialog() {
         return dialog;
@@ -72,28 +87,39 @@ public class AddPeopleWindow {
     /**
      * Sets the dialog associated with this AddPeopleWindow.
      * 
-     * @param dialog the JDialog object representing the dialog
+     * @param dialog the JDialog object representing the dialog associated with this AddPeopleWindow object
+     * @see JDialog
      * @author Cesar Bianchi
+     * @since 1.0
+     * @createdate 2022-02-01
      */
     public void setDialog(JDialog dialog) {
         this.dialog = dialog;
     }
 
     /**
-     * Returns the panel associated with this AddPeopleWindow.
+     * Retrieves the instance of AddPeoplePanel used by the AddPeopleWindow.
      * 
-     * @return the AddPeoplePanel object representing the panel
+     * @return The instance of AddPeoplePanel used by the AddPeopleWindow.
      * @author Cesar Bianchi
+     * @since 1.0
+     * @createdate 2022-02-01
      */
     public AddPeoplePanel getPanel() {
         return panel;
     }
 
     /**
-     * Sets the panel associated with this AddPeopleWindow.
+     * Sets the instance of AddPeoplePanel used by the AddPeopleWindow.
      * 
-     * @param panel the AddPeoplePanel object representing the panel
+     * @param panel The instance of AddPeoplePanel to be used by the AddPeopleWindow.
      * @author Cesar Bianchi
+     * @see AddPeoplePanel
+     * @see ConnectJellyfinAPI
+     * @see JellyfinPeopleItem
+     * @see JDialog
+     * @see Window
+     * @see SwingUtilities
      */
     public void setPanel(AddPeoplePanel panel) {
         this.panel = panel;
