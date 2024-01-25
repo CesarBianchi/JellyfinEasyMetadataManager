d6aee5e6cd93d797df34a1b1389e696d9a33e356
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-25 18:24:01 -0300

    added javaDoc

--- src/main/java/com/lariflix/jemm/dtos/JellyfinFolders.java
+++ src/main/java/com/lariflix/jemm/dtos/JellyfinFolders.java
@@ -3,12 +3,20 @@ package com.lariflix.jemm.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.ArrayList;
 
+
 /**
+ * This class represents the JellyfinFolders.
  *
- * @author cesarbianchi
+ * @author Cesar Bianchi
+ * @since 1.0
+ * @see JellyfinItems
+ * @see JellyfinFolder
+ * @see JellyfinMedia
+ * @see JellyfinMediaInfo
+ * @see JellyfinUser
+ * @see JellyfinUsers
  */
 public class JellyfinFolders {
-    
     @JsonProperty("Items") 
     public ArrayList<JellyfinFolder> items;
     @JsonProperty("TotalRecordCount") 
@@ -16,29 +24,77 @@ public class JellyfinFolders {
     @JsonProperty("StartIndex") 
     public int startIndex;
 
+    /**
+     * Default constructor for JellyfinFolders.
+     *
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public JellyfinFolders() {
     }
 
+    /**
+     * Gets the items.
+     *
+     * @return An ArrayList of JellyfinFolder objects representing the items.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public ArrayList<JellyfinFolder> getItems() {
         return items;
     }
 
+    /**
+     * Sets the items.
+     *
+     * @param items An ArrayList of JellyfinFolder objects containing the new items.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public void setItems(ArrayList<JellyfinFolder> items) {
         this.items = items;
     }
 
+    /**
+     * Gets the total record count.
+     *
+     * @return An integer representing the total record count.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public int getTotalRecordCount() {
         return totalRecordCount;
     }
 
+    /**
+     * Sets the total record count.
+     *
+     * @param totalRecordCount An integer containing the new total record count.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public void setTotalRecordCount(int totalRecordCount) {
         this.totalRecordCount = totalRecordCount;
     }
 
+    /**
+     * Gets the start index.
+     *
+     * @return An integer representing the start index.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public int getStartIndex() {
         return startIndex;
     }
 
+    /**
+     * Sets the start index.
+     *
+     * @param startIndex An integer containing the new start index.
+     * @author Cesar Bianchi
+     * @since 1.0
+     */
     public void setStartIndex(int startIndex) {
         this.startIndex = startIndex;
     }
