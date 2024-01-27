cbd73daf95ccefb4f1b3734df36703cfc2b4b71e
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-26 23:13:26 -0300

    Created new test cases

---
+++ src/test/java/com/lariflix/jemm/forms/AddGenrePanelTest.java
@@ -0,0 +1,28 @@
+package com.lariflix.jemm.forms;
+
+import com.lariflix.jemm.forms.AddGenrePanel;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddGenrePanelTest {
+
+    @Test
+    public void testAddGenrePanel() {
+        AddGenrePanel addGenrePanel = new AddGenrePanel();
+
+        // Test that the AddGenrePanel is not null
+        assertNotNull(addGenrePanel);
+    }
+
+    @Test
+    public void testExistingGenreSelected() {
+        // Test that the existingGenreSelected static variable is correctly initialized
+        assertEquals(1, AddGenrePanel.existingGenreSelected);
+    }
+
+    @Test
+    public void testNewGenreSelected() {
+        // Test that the newGenreSelected static variable is correctly initialized
+        assertEquals(2, AddGenrePanel.newGenreSelected);
+    }
+}
