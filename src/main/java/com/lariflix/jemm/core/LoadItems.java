5ff45f4520babdd7735bdc8f48d443f383dbf298
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-26 22:30:48 -0300

    added javaDoc

--- src/main/java/com/lariflix/jemm/core/LoadItems.java
+++ src/main/java/com/lariflix/jemm/core/LoadItems.java
@@ -11,9 +11,12 @@ import java.net.URL;
 import java.util.Scanner;
 import org.json.simple.parser.ParseException;
 
-/**
+ /**
+ * This class is responsible for loading items from the Jellyfin server.
  *
- * @author cesarbianchi
+ * @author Cesar Bianchi
+ * @since 1.0
+ * @see LoadItems
  */
 public class LoadItems {
 
@@ -23,9 +26,26 @@ public class LoadItems {
     private String cParentID = new String();
     private String fullURL = new String();
 
+    /**
+     * Default constructor for the LoadItems class.
+     *
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public LoadItems() {
+        // ...
     }
-    
+
+    /**
+     * Constructor for the LoadItems class.
+     *
+     * @param jellyfinURL The URL of the Jellyfin server.
+     * @param apiToken The API token for accessing the Jellyfin server.
+     * @param cAdminID The ID of the admin user.
+     * @param cParID The ID of the parent item.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public LoadItems(String jellyfinURL, String apiToken, String cAdminID, String cParID) {
        this.setJellyfinInstanceUrl(jellyfinURL);
        this.setApiToken(apiToken);
@@ -33,6 +53,16 @@ public class LoadItems {
        this.setcParentID(cParID);
     }
     
+    /**
+     * Requests items from the Jellyfin server.
+     *
+     * @return A JellyfinItems object containing the items.
+     * @throws MalformedURLException If the provided URL is not valid.
+     * @throws IOException If an I/O error occurs.
+     * @throws ParseException If there is an error parsing the server's response.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public JellyfinItems requestItems() throws MalformedURLException, IOException, ParseException{
         
         URL url = new URL(this.getFullURL());
@@ -72,38 +102,101 @@ public class LoadItems {
     }
     
     
+    /**
+     * Gets the Jellyfin instance URL.
+     *
+     * @return The Jellyfin instance URL.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getJellyfinInstanceUrl() {
         return jellyfinInstanceUrl;
     }
 
+    /**
+     * Sets the Jellyfin instance URL.
+     *
+     * @param jellyfinInstanceUrl The Jellyfin instance URL.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setJellyfinInstanceUrl(String jellyfinInstanceUrl) {
         this.jellyfinInstanceUrl = jellyfinInstanceUrl;
     }
 
+    /**
+     * Gets the API token.
+     *
+     * @return The API token.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getApiToken() {
         return apiToken;
     }
 
+    /**
+     * Sets the API token.
+     *
+     * @param apiToken The API token.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setApiToken(String apiToken) {
         this.apiToken = apiToken;
     }
 
+    /**
+     * Gets the ID of the admin user.
+     *
+     * @return The ID of the admin user.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getcUserAdminID() {
         return cUserAdminID;
     }
 
+    /**
+     * Sets the ID of the admin user.
+     *
+     * @param cUserAdminID The ID of the admin user.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setcUserAdminID(String cUserAdminID) {
         this.cUserAdminID = cUserAdminID;
     }
 
+    /**
+     * Gets the ID of the parent item.
+     *
+     * @return The ID of the parent item.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getcParentID() {
         return cParentID;
     }
 
+    /**
+     * Sets the ID of the parent item.
+     *
+     * @param cParentID The ID of the parent item.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setcParentID(String cParentID) {
         this.cParentID = cParentID;
     }
 
+    /**
+     * Constructs the full URL for accessing the items on the Jellyfin server.
+     *
+     * @return The full URL as a string.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getFullURL() {
         String urlWithApiKey = new String();
         
--- src/main/java/com/lariflix/jemm/core/LoadPeople.java
+++ src/main/java/com/lariflix/jemm/core/LoadPeople.java
@@ -12,8 +12,11 @@ import java.util.Scanner;
 import org.json.simple.parser.ParseException;
 
 /**
+ * This class is responsible for loading people from the Jellyfin server.
  *
- * @author cesarbianchi
+ * @author Cesar Bianchi
+ * @since 1.0
+ * @see LoadPeople
  */
 public class LoadPeople {
     private String jellyfinInstanceUrl = new String();
@@ -22,14 +25,39 @@ public class LoadPeople {
     private String cParentID = new String();
     private String fullURL = new String();
 
+    /**
+     * Default constructor for the LoadPeople class.
+     *
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public LoadPeople() {
+        // ...
     }
-    
+
+    /**
+     * Constructor for the LoadPeople class.
+     *
+     * @param jellyfinURL The URL of the Jellyfin server.
+     * @param apiToken The API token for accessing the Jellyfin server.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public LoadPeople(String jellyfinURL, String apiToken) {
        this.setJellyfinInstanceUrl(jellyfinURL);
        this.setApiToken(apiToken);
     }
 
+    /**
+     * Requests people from the Jellyfin server.
+     *
+     * @return A JellyfinCadPeopleItems object containing the people.
+     * @throws MalformedURLException If the provided URL is not valid.
+     * @throws IOException If an I/O error occurs.
+     * @throws ParseException If there is an error parsing the server's response.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public JellyfinCadPeopleItems requestPeople() throws MalformedURLException, IOException, ParseException{
         
         URL url = new URL(this.getFullURL());
@@ -69,38 +97,101 @@ public class LoadPeople {
     }
     
     
+    /**
+     * Gets the Jellyfin instance URL.
+     *
+     * @return The Jellyfin instance URL.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getJellyfinInstanceUrl() {
         return jellyfinInstanceUrl;
     }
 
+    /**
+     * Sets the Jellyfin instance URL.
+     *
+     * @param jellyfinInstanceUrl The Jellyfin instance URL.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setJellyfinInstanceUrl(String jellyfinInstanceUrl) {
         this.jellyfinInstanceUrl = jellyfinInstanceUrl;
     }
 
+    /**
+     * Gets the API token.
+     *
+     * @return The API token.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getApiToken() {
         return apiToken;
     }
 
+    /**
+     * Sets the API token.
+     *
+     * @param apiToken The API token.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setApiToken(String apiToken) {
         this.apiToken = apiToken;
     }
 
+    /**
+     * Gets the ID of the admin user.
+     *
+     * @return The ID of the admin user.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getcUserAdminID() {
         return cUserAdminID;
     }
 
+    /**
+     * Sets the ID of the admin user.
+     *
+     * @param cUserAdminID The ID of the admin user.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setcUserAdminID(String cUserAdminID) {
         this.cUserAdminID = cUserAdminID;
     }
 
+    /**
+     * Gets the ID of the parent item.
+     *
+     * @return The ID of the parent item.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getcParentID() {
         return cParentID;
     }
 
+    /**
+     * Sets the ID of the parent item.
+     *
+     * @param cParentID The ID of the parent item.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public void setcParentID(String cParentID) {
         this.cParentID = cParentID;
     }
 
+    /**
+     * Constructs the full URL for accessing the persons on the Jellyfin server.
+     *
+     * @return The full URL as a string.
+     * @since 1.0
+     * @author Cesar Bianchi
+     */
     public String getFullURL() {
         String urlWithApiKey = new String();
         
