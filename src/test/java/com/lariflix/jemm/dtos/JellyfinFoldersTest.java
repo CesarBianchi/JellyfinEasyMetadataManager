20aa19c689d006a3dffdfa47f9bd385f1d18d93e
Cesar Bianchi <cesar_bianchi@hotmail.com>
2024-01-29 21:52:31 -0300

    added Java Test Case

---
+++ src/test/java/com/lariflix/jemm/dtos/JellyfinFoldersTest.java
@@ -0,0 +1,25 @@
+package com.lariflix.jemm.dtos;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+
+public class JellyfinFoldersTest {
+
+    @Test
+    public void testJellyfinFolders() {
+        JellyfinFolders folders = new JellyfinFolders();
+
+        ArrayList<JellyfinFolder> folderList = new ArrayList<>();
+        folders.setItems(folderList);
+        assertEquals(folderList, folders.getItems());
+
+        int totalRecordCount = 10;
+        folders.setTotalRecordCount(totalRecordCount);
+        assertEquals(totalRecordCount, folders.getTotalRecordCount());
+
+        int startIndex = 5;
+        folders.setStartIndex(startIndex);
+        assertEquals(startIndex, folders.getStartIndex());
+    }
+}
---
+++ src/test/java/com/lariflix/jemm/dtos/JellyfinItemsTest.java
@@ -0,0 +1,25 @@
+package com.lariflix.jemm.dtos;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+
+public class JellyfinItemsTest {
+
+    @Test
+    public void testJellyfinItems() {
+        JellyfinItems items = new JellyfinItems();
+
+        ArrayList<JellyfinItem> itemList = new ArrayList<>();
+        items.setItems(itemList);
+        assertEquals(itemList, items.getItems());
+
+        int totalRecordCount = 10;
+        items.setTotalRecordCount(totalRecordCount);
+        assertEquals(totalRecordCount, items.getTotalRecordCount());
+
+        int startIndex = 5;
+        items.setStartIndex(startIndex);
+        assertEquals(startIndex, items.getStartIndex());
+    }
+}
