88f188f6e50bd6733b43d5b25bd6325e7ba2fb26
Cesar Bianchi <cesar_bianchi@hotmail.com>
2025-01-03 23:16:00 -0300

    Created Tools Menu
    Tools Menu created, with Export and Import Options

--- src/main/java/com/lariflix/jemm/utils/JemmVersion.java
+++ src/main/java/com/lariflix/jemm/utils/JemmVersion.java
@@ -25,7 +25,7 @@ public class JemmVersion {
      
      */
     public String getVersion() {
-        this.setVersion("v1.1.3");
+        this.setVersion("v1.2.0");
         return version;
     }
 
